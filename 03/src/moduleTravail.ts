8e41f2f978234698f720d2bb585e4754c91272c2
Ruoxuan Yang <ruoxuan.yang@telecom-paris.fr>
2024-03-02 15:28:57 +0100

    il faut suivre les consignes :3

--- 03/src/moduleTravail.ts
+++ 03/src/moduleTravail.ts
@@ -1,12 +1,3 @@
 export function calculerJoursRestants(deja: number) {
-    var wholeWeek = Math.floor(deja/7);
-    var daysLeft = deja%7;
-    var dejaTravail;
-    if (daysLeft <= 5){
-        dejaTravail =  wholeWeek*5 + daysLeft;
-    }
-    else {
-        dejaTravail = (wholeWeek+1)*5;
-    }
-    return 52*5 - dejaTravail;
-  }
+    return 52*5 - deja;
+}
--- 03/src/moduleVacances.ts
+++ 03/src/moduleVacances.ts
@@ -1,12 +1,3 @@
 export function calculerJoursRestants(deja: number) {
-  var wholeWeek = Math.floor(deja/7);
-  var daysLeft = deja%7;
-  var dejaVacances;
-  if (daysLeft <= 5){
-      dejaVacances =  wholeWeek*2;
-  }
-  else {
-      dejaVacances = wholeWeek*2 + 7 - daysLeft;
-  }
-  return 52*2 - dejaVacances;
+    return 52*2 - deja;
 }
